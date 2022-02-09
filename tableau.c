9eab0fea74cd818d9223de3a922438014c9e2e19
ichou <aichou.pro@gmail.com>
2022-02-09 18:07:14 +0100

    Affichage et remplissage d'un tableau

--- tableau.c
+++ tableau.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 
-int main () {
-int i,j;
-int tab[2][3];
-for (i=0; i<2;i++){
-	for (j=0;j<3;j++){
-		printf("Element %d %d : " , i ,j );
-		scanf("%d",&tab[i][j]);
+int main () 
+{
+	int i,j;
+	int tab[2][3];
+	for (i=0; i<2;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+			printf("Element %d %d : " , i ,j );
+			scanf("%d",&tab[i][j]);
 		}	
+	}
+	printf("Le tableau saisi est \n");
+	for(i=0;i<5;i++)
+	{
+		printf("%d :" ,tab[i]);
 	}
 }
